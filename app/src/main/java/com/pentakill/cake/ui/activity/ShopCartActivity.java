496ce3be99a31240b28a37462da7f86d50b21527
unknown <>
2016-01-30 00:11:57 +0800

    提交

---
+++ app/src/main/java/com/pentakill/cake/ui/activity/ShopCartActivity.java
@@ -0,0 +1,14 @@
+package com.pentakill.cake.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopCartActivity extends AppCompatActivity {
+
+    private String A;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_cart);
+    }
+}
---
+++ app/src/main/java/com/pentakill/cake/ui/adapter/Simple.java
@@ -0,0 +1,8 @@
+package com.pentakill.cake.ui.adapter;
+
+/**
+ * Created by Lenovo on 2016-1-29-0029.
+ */
+public class Simple {
+    //占位
+}
